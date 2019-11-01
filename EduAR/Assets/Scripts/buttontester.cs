fb54c7a1cd0136a8a6de97b5b0ed7ff1a91e2de1
pleij <pleij@DESKTOP-H6V7QV8>
2019-11-01 12:16:49 +0100

    interface verbeteringen

---
+++ EduAR/Assets/Scripts/buttontester.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class buttontester : MonoBehaviour {
+    int buttonPressed;
+    public void Test(Text text) {
+        buttonPressed++;
+        text.text = "Button pressed " + buttonPressed + " times";
+    }
+}
