9104207b268a4a10f8c54eee483993ce8953c7ae
Tim Meermans <timmeermans@live.nl>
2019-12-03 11:36:43 +0100

    changed AR package from Vuforia to ARFoundation

---
+++ EduARApp/TestingARFoundation/Assets/Scripts/ARInteraction.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.ARFoundation;
+
+public class ARInteraction : MonoBehaviour {
+    [SerializeField]
+    private GameObject ui;
+    [SerializeField]
+    private Text feedbackText;
+    [SerializeField]
+    private Toggle[] toggles;
+    private float text_time;
+ 
+    private ARSessionOrigin arOrigin;
+    private ARRaycastManager raycaster;
+
+    void Start() {
+        arOrigin = FindObjectOfType<ARSessionOrigin>();
+        raycaster = arOrigin.GetComponent<ARRaycastManager>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began) {
+            Ray raycast = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+            RaycastHit raycastHit;
+
+            if (Physics.Raycast(raycast, out raycastHit, maxDistance: 1000f)) {
+                if (raycastHit.collider.CompareTag("TestContent")) {
+                    ui.SetActive(true);
+                }
+            }
+        }
+
+        
+        if(ui.activeInHierarchy && feedbackText.text != "" && (Time.time - text_time == 2.5f)) {
+            feedbackText.text = "";
+        }
+    }
+
+    public void GoButtonPressed() {
+        foreach(Toggle toggle in toggles) {
+            if(toggle.isOn && toggle.CompareTag("Player")) {
+                feedbackText.color = Color.green;
+                feedbackText.text = "Correct!";
+                text_time = Time.time;
+            } else {
+                feedbackText.color = Color.red;
+                feedbackText.text = "Too bad peanutbutter";
+                text_time = Time.time;
+            }
+        }
+    }
+}
---
+++ EduARApp/TestingARFoundation/Assets/Scripts/ARTapToPlaceObject.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+using System;
+using UnityEngine.UI;
+
+public class ARTapToPlaceObject : MonoBehaviour {
+    public GameObject placementIndicator;
+    public GameObject contentToPlace;
+
+    //public Text raycastText;
+    //public Text poseText;
+    //public Text IndicatorText;
+    //public Text IsValidText;
+
+    private ARSessionOrigin arOrigin;
+    private ARRaycastManager raycaster;
+    private Pose placementPose;
+    private bool placementPoseIsValid = false;
+    private bool isPlaced = false;
+
+    void Start() {
+        arOrigin = FindObjectOfType<ARSessionOrigin>();
+        raycaster = arOrigin.GetComponent<ARRaycastManager>();
+    }
+    
+    void Update() {
+        UpdatePlacementPose();
+        UpdatePlacementIndicator();
+
+        if(placementPoseIsValid && Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began && !isPlaced) {
+            PlaceObject();
+        } 
+    }
+
+    private void PlaceObject() {
+        Instantiate(contentToPlace, placementPose.position, placementPose.rotation);
+        isPlaced = true;
+    }
+
+    private void UpdatePlacementIndicator() {
+        if (placementPoseIsValid) {
+            placementIndicator.SetActive(true);
+            placementIndicator.transform.SetPositionAndRotation(placementPose.position, placementPose.rotation);
+            //IndicatorText.text = "is active";
+        } else {
+            placementIndicator.SetActive(false);
+            //IndicatorText.text = "not active";
+        }
+    }
+
+    private void UpdatePlacementPose() {
+        var screenCenter = Camera.main.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
+        var hits = new List<ARRaycastHit>();
+        raycaster.Raycast(screenCenter, hits, TrackableType.Planes);
+
+        placementPoseIsValid = hits.Count > 0;
+        //IsValidText.text = placementPoseIsValid.ToString();
+
+        if (placementPoseIsValid) {
+            //raycastText.text = "Sending raycast, hit: " + hits[0].hitType + "\n Hits Count: " + hits.Count;
+            placementPose = hits[0].pose;
+
+            var cameraForward = Camera.main.transform.forward;
+            var cameraBearing = new Vector3(cameraForward.x, 0f, cameraForward.z).normalized;
+
+            placementPose.rotation = Quaternion.LookRotation(cameraBearing);
+        }
+    }
+}
